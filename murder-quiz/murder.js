b27101828d105000d81af4a44c5dd72c21b54949
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-10 19:22:38 -0500

    murder quiz var solved

---
+++ murder-quiz/murder.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (/* your conditional goes here */) {
+
+} else if (/* your conditional goes here */) {
+
+} else if (/* your conditional goes here */) {
+
+} else {
+
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
