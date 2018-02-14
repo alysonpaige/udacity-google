173307765d737f237a1243a6613c1ec846d6e2a1
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-14 09:48:29 -0500

    error quiz 1

---
+++ fix-error/error1.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+// fix the for loop
+// supposed to print the numbers 5 through 9
+for (x < 10; x++) {
+    console.log(x);
+}
+
+// fixed
+for (var x = 5; x <= 9; x++) {
+    console.log(x);
+}
