4e24037ef79adf8d5a1134b7a711461f977aae54
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-16 13:30:40 -0500

    named function

---
+++ cry/cry.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ */
+
+ /*
+ Write a named function expression that stores the function in a variable called cry and returns "boohoo!"
+ */
+
+var cry = function sad(x) {
+  return "boohoo!";
+}
+
+console.log(cry());
