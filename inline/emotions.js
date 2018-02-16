33ab36682d89769ac95e15539724318edbd6000b
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-16 13:50:08 -0500

    inline function

--- inline/emotions.js
+++ inline/emotions.js
@@ -2,11 +2,22 @@
  * Programming Quiz: Inline Functions (5-6)
  */
 
-// don't change this code
 function emotions(myString, myFunc) {
     console.log("I am " + myString + ", " + myFunc(2));
 }
 
-// your code goes here
-// call the emotions function here and pass in an
-// inline function expression
+emotions("happy", function laugh(num) {
+  var ha = "ha";
+  return ha.repeat(num) + "!";
+});
+
+/*
+with for loop:
+emotions(“happy”, function laugh(x) {
+  var ha = "";
+  for (var x = 0; x < num; x++) {
+    ha += "ha";
+  }
+  return ha + "!";
+});
+*/
