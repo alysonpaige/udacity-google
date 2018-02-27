a19b47646b6bc0a9addf2dc202708dbf63b5fc57
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-27 08:35:27 -0500

    dom tree family2

--- DOM/family-tree/dom-tree.js
+++ DOM/family-tree/dom-tree.js
@@ -5,20 +5,12 @@ For this quiz, you'll need to add to the DOM tree that already exists.
 of '#family2'. '#bruce' should have two <div>s as children, '#madison' and '#hunter'.
 */
 
-var family1, family2, madison, bruce, hunter;
-
-family1 = $(".family1");
-family2 = $("<div id="#family2"><h1>Family2</h1></div>");
-bruce = $("<div id="#bruce"><h2>Bruce</h2></div>");
-madison = $("<div id="#madison"><h3>Madison</h3></div>");
-hunter =$("<div id="#hunter"><h3>Hunter</h3></div>");
-
-family2.insertAfter(family1);
-bruce.append(family2);
-madison.append(bruce);
-hunter.insertAfter(madison);
+$("<div id='family2'><h1>Family 2</h1></div>").insertAfter("#family1");
+$("#family2").append("<div id='bruce'><h2>Bruce</h2></div>");
+$("#bruce").append("<div id='madison'><h3>Madison</h3></div>");
+$("<div id='hunter'><h3>Hunter</h3></div>").insertAfter("#madison");
 
 // $("#family1").parent().append('<div id="family2"><h1>Family 2 </h1></div>');
 // $('#family2').append("<div id='bruce'><h2>Bruce</h2></div>");
 // $("#bruce").append("<div id='madison'><h3>Madison</h3></div>");
-// $('#madison').append("<div id='hunter'><h3>Hunter</h3></div>")
+// $('#madison').append("<div id='hunter'><h3>Hunter</h3></div>");
