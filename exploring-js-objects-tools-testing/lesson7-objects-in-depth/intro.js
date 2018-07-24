cb8c8bcc9934bda7d26a52b89e94ec68fb21dbda
Alyson Paige <alysonstiffel@gmail.com>
2018-07-24 12:17:49 -0400

    intro array lesson 7

---
+++ exploring-js-objects-tools-testing/lesson7-objects-in-depth/intro.js
@@ -0,0 +1,11 @@
+/*
+Recall that arrays can store many different types of data, not just strings!
+Below, create an array called `mixedArray` that contains:
+
+* A number
+* A string
+* A boolean
+* Another array
+*/
+
+var mixedArray = [3, 'soccer', true, [1, 2]];
