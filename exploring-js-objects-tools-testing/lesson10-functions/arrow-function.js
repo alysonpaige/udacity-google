9136951228e614e83c24f4723383f1870979474f
Alyson Paige <alysonstiffel@gmail.com>
2018-08-03 16:02:49 -0400

    arrow function quiz

--- exploring-js-objects-tools-testing/lesson10-functions/arrow-function.js
+++ exploring-js-objects-tools-testing/lesson10-functions/arrow-function.js
@@ -3,8 +3,6 @@
  */
 
 // convert to an arrow function
-const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function(square) {
-	return square * square;
-});
+const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map( square => square * square );
 
 console.log(...squares);
